096ab49e6da36b55303665b4bde9b877ad0f2e4b
Anthony D'Amato <anthony.damato@hotmail.fr>
2020-02-13 15:03:50 +0100

    Init react app (#2)
    * create github action for Yarn
    
    * init react app with typescript
    
    * improve README.md
    
    * use fixed package versions in package.json
    
    * clean page + test
    
    * try to test all

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to PetitRoBERT.<br />
+          🚧👷🏻‍♂️🚧
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
