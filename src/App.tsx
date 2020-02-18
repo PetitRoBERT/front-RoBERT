acf8f0411d3a29488dbb8785444cb34dc70df4a8
Anthony D'Amato <anthony.damato@hotmail.fr>
2020-02-18 13:05:11 +0100

    add emotion with an example in front page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import styled from '@emotion/styled';
+
+type ButtonProps = {
+  counter: number;
+};
+
+const StyledButtonTest = styled.button<ButtonProps>`
+  width: ${({ counter }) => ((counter + 4) * 10) % 200}px;
+`;
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = () => setCounter(counter + 1);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +22,9 @@ const App = () => {
           <br />
           🚧👷🏻‍♂️🚧
         </p>
+        <StyledButtonTest onClick={incrementCounter} counter={counter}>
+          Test
+        </StyledButtonTest>
       </header>
     </div>
   );
